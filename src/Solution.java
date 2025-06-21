977b37158f8cf312320e135a6e28d753eb960361
Malik Johnson <60333731+MalikJohnson@users.noreply.github.com>
2025-06-20 23:03:34 -0400

    Changed solution
    The recursion method with fibonacci times out in the test cases on leetcode. Must use a different method such as DP dynamic programming.

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        // Recursion and Fibonacci
-        if(n<=2) return n;
-        return climbStairs(n-1) + climbStairs(n-2);
+        // Recursion and Fibonacci timed out
+        if (n <= 2) return n;
+        int one = 1; 
+        int two = 2; 
+        for (int i = 3; i <= n; i++) {
+            int temp = one + two;
+            one = two;
+            two = temp;
+        }
+        return two;
     }
 }
